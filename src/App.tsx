a587d8bcf7ce4191190f107b3d4a900202052d22
luzh <332862394@qq.com>
2021-12-13 14:46:23 +0800

    导航栏布局搞定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,76 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex: 1;
+`;
+const Nav = styled.div`
+  border: 1px solid orange;
+  > ul {
+    display: flex;
 
+    > li {
+      width: 33.3333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
 function App() {
   return (
-   <div>
-    Hello
-   </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Routes>
+            <Route path="/tags" element={<Tags />} />
+            <Route path="/money" element={<Money />} />
+            <Route path="/statistics" element={<Statistics />} />
+            <Route path="/" element={<Navigate to="/money" />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </Main>
+
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+function NoMatch() {
+  return <div>页面不存在</div>;
+}
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
 }
 
 export default App;
