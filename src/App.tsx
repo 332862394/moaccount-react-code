d51abc3860b42dfde731c5e7e97f3a1ce3a461ee
luzh <332862394@qq.com>
2021-12-14 10:22:58 +0800

    创建layout组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
 import React from "react";
-import styled from "styled-components";
 import {
   HashRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
-import Nav from "./components/Nav";
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex: 1;
-`;
-
+import Layout from "components/Layout";
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Routes>
-            <Route path="/tags" element={<Tags />} />
-            <Route path="/money" element={<Money />} />
-            <Route path="/statistics" element={<Statistics />} />
-            <Route path="/" element={<Navigate to="/money" />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-        </Main>
-        <Nav />
-      </Wrapper>
+      <Routes>
+        <Route path="/tags" element={<Tags />} />
+        <Route path="/money" element={<Money />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/" element={<Navigate to="/money" />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </Router>
   );
 }
@@ -38,15 +23,27 @@ function NoMatch() {
   return <div>页面不存在</div>;
 }
 function Statistics() {
-  return <h2>统计页面</h2>;
+  return (
+    <Layout>
+      <h2>统计页面</h2>
+    </Layout>
+  );
 }
 
 function Tags() {
-  return <h2>标签页面</h2>;
+  return (
+    <Layout>
+      <h2>标签页面</h2>
+    </Layout>
+  );
 }
 
 function Money() {
-  return <h2>记账页面</h2>;
+  return (
+    <Layout>
+      <h2>记账页面</h2>
+    </Layout>
+  );
 }
 
 export default App;
