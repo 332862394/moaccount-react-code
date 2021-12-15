e34daedcda582fb57bacba93d68e090e0b46d5f2
luzh <332862394@qq.com>
2021-12-15 17:18:52 +0800

    添加classname库/完成点击回退功能

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cs from "classnames";
 //require一个目录/文件夹
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
   requireContext.keys().forEach(requireContext);
@@ -10,10 +11,11 @@ try {
 
 type Props = {
   name?: string;
-};
+} & React.SVGAttributes<SVGElement>;
 const icon = (props: Props) => {
+  const { name, children, className, ...rest } = props;
   return (
-    <svg className="icon">
+    <svg className={cs("icon", className)} {...rest}>
       {props.name && <use xlinkHref={"#" + props.name} />}
     </svg>
   );
--- src/useTags.tsx
+++ src/useTags.tsx
@@ -2,6 +2,7 @@ import { createId } from "lib/createId";
 import { useState } from "react";
 import { identifier } from "@babel/types";
 import Tags from "./views/Tags";
+import { Tag } from "views/Tag";
 const defaultTags = [
   { id: createId(), name: "衣" },
   { id: createId(), name: "食" },
@@ -21,21 +22,11 @@ const useTags = () => {
     }
     return result;
   };
-  const updateTag = (id: number, obj: { name: string }) => {
-    const index = findTagIndex(id);
-    //深拷贝tags得到tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    //把tagsClone的第index删掉，换成{id:id,name:obj.name}
-    tagsClone.splice(index, 1, { id: id, name: obj.name });
-    setTags(tagsClone);
+  const updateTag = (id: number, { name }: { name: string }) => {
+    setTags(tags.map((tag) => (tag.id === id ? { id, name } : tag)));
   };
   const deleteTag = (id: number) => {
-    const index = findTagIndex(id);
-    //深拷贝tags得到tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    //把tagsClone的第index删掉，换成{id:id,name:obj.name}
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
+    setTags(tags.filter((tag) => tag.id !== id));
   };
   return { tags, setTags, findTag, updateTag, findTagIndex, deleteTag };
 };
