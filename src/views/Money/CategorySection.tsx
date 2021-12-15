15d7083a720ef2593c53ae278125825d5fbd7d1f
luzh <332862394@qq.com>
2021-12-15 09:40:21 +0800

    category q切换功能搞定

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,22 +1,23 @@
-import styled from 'styled-components';
+import React, { useRef, useState } from "react";
+import styled from "styled-components";
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   font-size: 24px;
-  > ul{
-    display:flex;
-    background:#c4c4c4;
+  > ul {
+    display: flex;
+    background: #c4c4c4;
     > li {
-      width: 50%; 
-      text-align:center;
+      width: 50%;
+      text-align: center;
       padding: 16px 0;
-      position:relative;
-      &.selected::after{
-        content: '';
-        display:block; 
+      position: relative;
+      &.selected::after {
+        content: "";
+        display: block;
         height: 3px;
-        background:#333;
-        position:absolute;
-        bottom:0;
+        background: #333;
+        position: absolute;
+        bottom: 0;
         width: 100%;
         left: 0;
       }
@@ -24,4 +25,27 @@ const CategorySection = styled.section`
   }
 `;
 
-export {CategorySection}
+const CategorySection: React.FC = () => {
+  const categoryMap = { "-": "支出", "+": "收入" };
+  type Keys = keyof typeof categoryMap;
+  const [categoryList] = useState<Keys[]>(["-", "+"]);
+  const [category, setCategory] = useState("-"); //+表示收入 -表示支出
+
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map((e) => (
+          <li
+            className={category === e ? "selected" : ""}
+            onClick={() => {
+              setCategory(e);
+            }}
+          >
+            {categoryMap[e]}
+          </li>
+        ))}
+      </ul>
+    </Wrapper>
+  );
+};
+export { CategorySection };
