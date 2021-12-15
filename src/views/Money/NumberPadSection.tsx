734b6f83cc203dfc16768de6416ab8dd015be886
luzh <332862394@qq.com>
2021-12-15 10:29:02 +0800

    数字组件功能基本完成

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 const Wrapper = styled.section`
   display: flex;
@@ -58,7 +58,15 @@ const Wrapper = styled.section`
   }
 `;
 const NumberPadSection: React.FC = () => {
-  const [output, setOutput] = useState("0");
+  const [output, _setOutput] = useState("0");
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16);
+    } else if (output.length === 0) {
+      output = "0";
+    }
+    _setOutput((t) => output);
+  };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) {
@@ -75,18 +83,27 @@ const NumberPadSection: React.FC = () => {
       case "7":
       case "8":
       case "9":
-      case ".":
         if (output === "0") {
-          setOutput((t) => text);
+          setOutput(text);
         } else {
-          setOutput((t) => output + text);
+          setOutput(output + text);
         }
         break;
+      case ".":
+        if (output.indexOf(".") >= 0) {
+          return;
+        }
+        setOutput(output + ".");
+        break;
       case "删除":
-        console.log("删除");
+        if (output.length === 1) {
+          setOutput("");
+        } else {
+          setOutput(output.slice(0, -1));
+        }
         break;
       case "清空":
-        console.log("清空");
+        setOutput("");
         break;
       case "OK":
         console.log("确认");
